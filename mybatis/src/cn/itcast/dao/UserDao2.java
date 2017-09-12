c19c239e4dbbb9e7a7e7921867fac4f4e26eb2ff
huaizhong <peng_huaizhong@163.com>
2017-09-12 21:02:44 +0800

    222

--- mybatis/src/cn/itcast/dao/UserDao2.java
+++ mybatis/src/cn/itcast/dao/UserDao2.java
@@ -8,4 +8,5 @@ public interface UserDao2 {
     public User selectUserById(Integer id) throws Exception;
     
     public List<User> selectUserByUsername(String name) throws Exception;
+    public List<User> selectUserByUsername1(String name) throws Exception;
 }
